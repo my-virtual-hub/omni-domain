4021afb7ef75d3d73ca5d8baaaed74c7170e0dc1
Marco Quiçula <marco.quicula@quicula.com.br>
2023-12-18 20:42:23 -0300

    Create First Javadoc

---
+++ docs/v0.1-0-SNAPSHOT/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"br.com.quicula.omnicommunicator.domain","l":"Main"},{"p":"br.com.quicula.omnicommunicator.domain.utils","l":"PrintBanner"}];updateSearchResults();
--- src/main/java/br/com/quicula/omnicommunicator/domain/exception/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains the custom domain exceptions of the OmniCommunicator. These exceptions
- * represent error conditions that are specific to the application's domain.
- */
-package br.com.quicula.omnicommunicator.domain.exception;
